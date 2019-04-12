200492cc9ab3b02edf9d676aa37d84db99761392
Sinclair Target <sinclairtarget@gmail.com>
2019-04-12 15:25:10 -0500

    Add bin and dummify functions to pipeline.

--- hw2/pipeline/clean.py
+++ hw2/pipeline/clean.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def impute(df, col, how='avg'):
     subst = np.nan
@@ -9,3 +10,14 @@ def impute(df, col, how='avg'):
         raise Exception(f"\"how\" argument of \"{how}\" is not supported.")
 
     df.loc[df[col].isnull(), col] = subst
+
+def bin(df, col, bins):
+    new_col = col + '_binned'
+    max_val = np.max(df[col])
+    df[new_col] = pd.cut(df[col], bins + [max_val], include_lowest=True)
+
+def dummify(df, col):
+    uniqs = pd.unique(df[col])
+    for uniq in uniqs:
+        new_col = col + '_is_' + str(uniq)
+        df[new_col] = df[col] == uniq
