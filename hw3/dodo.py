43a6c1844af423cd81d1af62b2571c6cdfc66605
Sinclair Target <sinclairtarget@gmail.com>
2019-05-01 14:39:37 -0500

    Use jupytext to generate analysis.

--- hw3/dodo.py
+++ hw3/dodo.py
@@ -1,7 +1,22 @@
-def task_generate_report():
-    """Generates PDF report from RMarkdown source."""
+DOIT_CONFIG = {
+    'default_tasks': ['generate_analysis_html']
+}
+
+def task_generate_analysis_html():
+    """Generates HTML Jupyter notebook file from notebook source."""
     return {
-        'file_dep': ['report.rmd'],
-        'targets': ['report.pdf'],
-        'actions': ['pipenv run Rscript -e \'rmarkdown::render("report.rmd")\'']
+        'file_dep': ['analysis.ipynb'],
+        'targets': ['analysis.html'],
+        'actions': ['pipenv run jupyter nbconvert --execute --to html analysis.ipynb'],
+        'clean': True
+    }
+
+
+def task_generate_analysis_notebook():
+    """Generates Jupyter notebook from markdown source."""
+    return {
+        'file_dep': ['analysis.md'],
+        'targets': ['analysis.ipynb'],
+        'actions': ['pipenv run jupytext --to notebook analysis.md'],
+        'clean': True                   # Remove all targets
     }
