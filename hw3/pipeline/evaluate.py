71faf2ef6324621026316f4406a4d009dbea1692
Sinclair Target <sinclairtarget@gmail.com>
2019-05-01 18:08:59 -0500

    Add split() pipeline function to create test and train data.

--- hw3/pipeline/evaluate.py
+++ hw3/pipeline/evaluate.py
@@ -1,7 +1,25 @@
 """
 This module contains functions that help evaluate trained models.
 """
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def split(df, label_colname, test_size, seed):
+    """
+    Splits a dataset into training and testing dataframes.
+    """
+    X = df.drop(columns=[label_colname]).values
+    y = df[label_colname].values
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=test_size, random_state=seed)
+
+    df_train = pd.DataFrame(np.column_stack((X_train, y_train)),
+                            columns=df.columns)
+    df_test = pd.DataFrame(np.column_stack((X_test, y_test)),
+                            columns=df.columns)
+    return df_train, df_test
+
 
 def accuracy(df, target_col, predict_col=None):
     """
